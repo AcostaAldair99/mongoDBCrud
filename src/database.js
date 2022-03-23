966dd2dcc6dbdb0a06b7ba76b88844213e33e392
Aldair Acosta <aldair.acostafm@gmail.com>
2022-03-23 14:08:44 -0600

    Terminado

--- src/database.js
+++ src/database.js
@@ -1,10 +1,11 @@
 import {connect} from "mongoose";
-
+import {MONGODB_URI} from "./config"
 //De esta forma no se necesita llamar a la funcion
 //para que esta se ejecute.
+
 (async()=>{
     try{
-        const db=await connect("mongodb://0.0.0.0/crud-mongo");
+        const db = await connect(MONGODB_URI);
         console.log("Db connected to",db.connection.name);
     }catch(error){
         console.log(error);
--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
+import { PORT } from './config.js';
 import app from './app.js'
 import './database.js'
+
+
 //Codigo para Arrancar la aplicacion
-app.listen(3000);
-console.log("Server on porasdt",27017);
+app.listen(PORT);
+console.log("Server on porasdt",PORT);
 
 
 
